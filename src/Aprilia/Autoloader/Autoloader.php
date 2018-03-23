857679844cdfecd7230ae4b85027b20ee3ec0066
Mitha Aprilia <mitha@mithaaprilia.com>
2018-03-23 08:59:57 +0700

    delete
    Signed-off-by: Mitha Aprilia <mitha@mithaaprilia.com>

--- src/Aprilia/Autoloader/Autoloader.php
+++ src/Aprilia/Autoloader/Autoloader.php
@@ -36,9 +36,16 @@
  * @link  https://www.mithaapp.com
  */
 
-namespace Mitha\Aprilia\Language;
-
-class Language
+class Autoloader
 {
-
+    public function register()
+    {
+        spl_autoload_register(function ($class) {
+            $root = dirname(__DIR__);   // get the parent directory
+            $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+            if (is_readable($file)) {
+                require $root . '/' . str_replace('\\', '/', $class) . '.php';
+            }
+        });
+    }
 }
